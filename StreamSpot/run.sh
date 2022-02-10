ca3905524c84826cc09750d9796da3ec7159a4ed
Jiaxi Li <jiaxili0319@gmail.com>
2022-02-10 21:49:07 +1100

    initial refactor

--- StreamSpot/run.sh
+++ StreamSpot/run.sh
@@ -14,13 +14,26 @@
 
 
 docker run \
-  -v /Users/jiaxili/Documents/study/code_repo/sbustreamspot-core:/app \
-  -v /Users/jiaxili/Documents/study/code_repo/sbustreamspot-data:/data \
-  -v /Users/jiaxili/Documents/study/code_repo/streamspot-bootstrap-clusters:/data/bootstrap \
+  -v /Users/jl102430/Documents/study/code_repo/my_anomaly_detection/baseline/sbustreamspot-core:/app \
+  -v /Users/jl102430/Documents/study/code_repo/my_anomaly_detection/baseline/sbustreamspot-data:/data \
+  -v /Users/jl102430/Documents/study/code_repo/my_anomaly_detection/baseline/streamspot-bootstrap-clusters:/data/bootstrap \
   streamspot \
-  /app/streamspot --edges=/data/all.tsv \
+  /app/streamspot --edges=/app/test_edges.txt \
              --bootstrap=/app/test_bootstrap_clusters.txt \
              --chunk-length=10 \
              --num-parallel-graphs=10 \
              --max-num-edges=10 \
              --dataset=all
+
+
+ docker run \
+   -v /Users/jiaxili/Documents/study/code_repo/sbustreamspot-core:/app \
+   -v /Users/jiaxili/Documents/study/code_repo/sbustreamspot-data:/data \
+   -v /Users/jiaxili/Documents/study/code_repo/streamspot-bootstrap-clusters:/data/bootstrap \
+   streamspot \
+   /app/streamspot --edges=/data/all.tsv \
+              --bootstrap=/app/test_bootstrap_clusters.txt \
+              --chunk-length=10 \
+              --num-parallel-graphs=10 \
+              --max-num-edges=10 \
+              --dataset=all
